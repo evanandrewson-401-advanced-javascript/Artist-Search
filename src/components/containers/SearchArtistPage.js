b11855570c00c40741d66c178df924a4caed58c6
evanandrewson <evanandrewson@gmail.com>
2019-11-07 14:44:42 -0800

    refactored SearchArtistPage to use hooks

--- src/components/containers/SearchArtistPage.js
+++ src/components/containers/SearchArtistPage.js
@@ -1,71 +1,130 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Search from '../searches/Search';
 import PagingButtons from '../paging/PagingButtons';
 import Artists from '../artists/Artists';
 import getArtists from '../services/getArtists';
 
-export default class SearchArtistPage extends Component {
-  state = {
-    searchInput: '',
-    artists: [],
-    page: 0,
-    upDisable: true,
-    downDisable: true
-  }
+const SearchArtistPage = () => {
+  const [searchInput, updateSearchInput] = useState('');
+  const [artists, updateArtists] = useState([]);
+  const [page, updatePage] = useState(0);
+  const [upDisabled, updateUpDisabled] = useState(true);
+  const [downDisabled, updateDownDisabled] = useState(true);
 
-  handleChange = (event) => {
-    this.setState({ searchInput: event.target.value });
-  }
+  const handleChange = (event) => {
+    updateSearchInput(event.target.value);
+  };
 
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    getArtists(this.state.searchInput, 0)
-      .then(result => this.setState({ 
-        artists: result, 
-        upDisable: false, 
-      }));
-  }
+    getArtists(searchInput, 0)
+      .then(result => { 
+        updateArtists(result); 
+        updateUpDisabled(false);
+        updatePage(0); 
+      });
+  };
 
-  pageUpFunction = () => {
-    this.setState(state => ({
-      page: state.page + 1
-    }), () => {
-      return getArtists(this.state.searchInput, this.state.page)
-        .then(result => this.setState({ 
-          artists: result,
-          downDisable: false
-        }));
-    });
-  } 
+  const pageUpFunction = () => {
+    updatePage(page + 1);
+    updateDownDisabled(false);
+  };
 
-  pageDownFunction = () => {
-    this.setState(state => ({
-      page: state.page - 1
-    }), () => {
-      return getArtists(this.state.searchInput, this.state.page)
-        .then(result => this.setState({ 
-          artists: result,
-          downDisable: this.state.page === 0 ? true : false
-        }));
-    });
-  }
+  const pageDownFunction = () => {
+    updatePage(page - 1);
+  };
 
-  render() {
-    return (
-      <>
-        <Search
-          searchInput={this.state.searchInput}
-          handleChange={this.handleChange}
-          handleSubmit={this.handleSubmit}
-        />
-        <PagingButtons 
-          pageUpFunction={this.pageUpFunction}
-          pageDownFunction={this.pageDownFunction}
-          upDisable={this.state.upDisable}
-          downDisable={this.state.downDisable}
-        />
-        <Artists items={this.state.artists}/>
-      </>
-    );
-  }
-}
+  useEffect(() => {
+    getArtists(searchInput, page)
+      .then(result => {
+        updateArtists(result); 
+      });
+
+    return updateDownDisabled(page == 0);
+  }, [page]);
+
+  return (
+    <>
+      <Search
+        searchInput={searchInput}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+      />
+      <PagingButtons 
+        pageUpFunction={pageUpFunction}
+        pageDownFunction={pageDownFunction}
+        upDisable={upDisabled}
+        downDisable={downDisabled}
+      />
+      <Artists items={artists}/>
+    </>
+  );
+};
+
+export default SearchArtistPage;
+
+// export default class SearchArtistPage extends Component {
+//   state = {
+//     searchInput: '',
+//     artists: [],
+//     page: 0,
+//     upDisable: true,
+//     downDisable: true
+//   }
+
+//   handleChange = (event) => {
+//     this.setState({ searchInput: event.target.value });
+//   }
+
+//   handleSubmit = (event) => {
+//     event.preventDefault();
+//     getArtists(this.state.searchInput, 0)
+//       .then(result => this.setState({ 
+//         artists: result, 
+//         upDisable: false, 
+//       }));
+//   }
+
+//   pageUpFunction = () => {
+//     this.setState(state => ({
+//       page: state.page + 1
+//     }), () => {
+//       return getArtists(this.state.searchInput, this.state.page)
+//         .then(result => this.setState({ 
+//           artists: result,
+//           downDisable: false
+//         }));
+//     });
+//   } 
+
+//   pageDownFunction = () => {
+//     this.setState(state => ({
+//       page: state.page - 1
+//     }), () => {
+//       return getArtists(this.state.searchInput, this.state.page)
+//         .then(result => this.setState({ 
+//           artists: result,
+//           downDisable: this.state.page === 0 ? true : false
+//         }));
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <>
+//         <Search
+//           searchInput={this.state.searchInput}
+//           handleChange={this.handleChange}
+//           handleSubmit={this.handleSubmit}
+//         />
+//         <PagingButtons 
+//           pageUpFunction={this.pageUpFunction}
+//           pageDownFunction={this.pageDownFunction}
+//           upDisable={this.state.upDisable}
+//           downDisable={this.state.downDisable}
+//         />
+//         <Artists items={this.state.artists}/>
+//       </>
+//     );
+//   }
+// }
